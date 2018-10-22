133d9b4dfd2fb7df1249230ef5e34a8c28aa6512
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-22 06:51:49 +0100

    Added all components and particles js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+
 import "./App.css";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 3,
+      density: {
+        enable: true,
+        value_area: 80
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particleOptions} />
         <Navigation />
-        {/* <Logo/>
-        <ImageLinkForm/>
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* 
+        
        <FaceRecognition/>*/}
       </div>
     );
