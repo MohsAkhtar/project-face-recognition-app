b21a877ff6fe892d1dd7c1da1d2255a0d81269b4
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-10-25 13:10:16 +0100

    Updates

---
+++ precache-manifest.cbd827e7d5ab8057ed0e52f39e987ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7c6ee839b23ca3d30e",
+    "url": "/project-face-recognition-app/static/js/runtime~main.0b7c6ee8.js"
+  },
+  {
+    "revision": "a2f6480139fc2ed56c3a",
+    "url": "/project-face-recognition-app/static/js/main.a2f64801.chunk.js"
+  },
+  {
+    "revision": "a116f2f4850c57a10da1",
+    "url": "/project-face-recognition-app/static/js/1.a116f2f4.chunk.js"
+  },
+  {
+    "revision": "a2f6480139fc2ed56c3a",
+    "url": "/project-face-recognition-app/static/css/main.331d49a5.chunk.css"
+  },
+  {
+    "revision": "a116f2f4850c57a10da1",
+    "url": "/project-face-recognition-app/static/css/1.831ede21.chunk.css"
+  },
+  {
+    "revision": "24a4923eb78f1d178c734791ba39c224",
+    "url": "/project-face-recognition-app/index.html"
+  }
+];
